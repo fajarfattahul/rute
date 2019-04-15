137b0e7ada47b3a89d94dcc0b3b2f2902c5686c3
NoName03 <williamafif123@gmail.com>
2019-04-15 16:17:38 +0700

    relasi pemasaran

--- app/Lokasi.php
+++ app/Lokasi.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Lokasi extends Model
 {
     protected $fillable = ['nama_lokasi', 'wilayah', 'waktu_pemesanan'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'pemasarans', 'id_lokasi', 'id_sales');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Lokasi;
 
 class User extends Authenticatable
 {
@@ -36,6 +37,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function lokasis()
+    {
+        return $this->belongsToMany(Lokasi::class, 'pemasarans', 'id_sales', 'id_lokasi');
+    }
 
     public function admin()
     {
